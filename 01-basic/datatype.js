83df11f82d728dd08f8e1004bf2c56eeef359319
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-09-15 12:37:20 +0000

    datatype

---
+++ 01-basic/datatype.js
@@ -0,0 +1,26 @@
+//"use strict"; // tread all js code in new version
+
+// alert(3 + 3) // we are useing nodejs, not browser
+console.log(3 
+    + 3); //code readability should be high
+console.log("Priya");
+
+let name = "priya"
+let age = 21
+let isLoggedIn =true
+let state = null;
+
+//Number => 2to power 53
+//Bigint
+//String => ""
+//Bollean => True/False
+//null // standaloan value
+//undefined
+//Symbol
+
+//Object
+
+console.log(typeof "priya");
+console.log(typeof 21);
+console.log(typeof true);
+console.log(typeof null);
