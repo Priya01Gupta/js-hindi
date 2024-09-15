d1b920ca6edbb9b25c43fc5621112dfdfcd6c7d9
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-09-15 12:36:56 +0000

    variable

---
+++ 01-basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 123456
+let accountEmail = "Priya1@gmail.com"
+var accountPassword = 24680
+accountCity = "Hamirpur"
+
+// accountId = 135791
+
+//console.log(accountId);
+accountEmail = "rath@gmail.com"
+accountPassword = 15936
+accountCity = "Rath"
+
+/*
+perfer var is not used
+beacuse of issue to block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword, accountCity]);
