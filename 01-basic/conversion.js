14e1c58b644db392a7ff06070e766d0a904a52c6
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-09-17 14:02:39 +0000

    conversion.js

---
+++ 01-basic/conversion.js
@@ -0,0 +1,29 @@
+let score = 33
+let score1 = "33abc"
+let score2 = null
+let score3 = undefined
+
+console.log(typeof score);
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof (score));
+console.log(typeof (score1));
+console.log(typeof (score2));
+console.log(typeof (score3));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
