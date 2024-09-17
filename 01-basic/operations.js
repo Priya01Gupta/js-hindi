05abeff0ede218a4be3574752476f02aa3efe9ed
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-09-17 14:53:17 +0000

    operation.js

---
+++ 01-basic/operations.js
@@ -0,0 +1,37 @@
+let value = 3
+
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = 3
+let str2 = 4
+str3 = str1 + str2
+
+// console.log(str3);
+// console.log(1 + "3");
+// console.log("1" + 3);
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log( (2 + 3) * (3 % 3));
+
+// console.log(true);
+// console.log(+true);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter1 = 100
+++gameCounter1;
+console.log(gameCounter1);
+
+let gameCounter2 = 100
+gameCounter2++;
+console.log(gameCounter2);
