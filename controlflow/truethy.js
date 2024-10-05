a43d14d339381d92571a661e0590cc357dad0826
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-10-05 13:34:57 +0000

    truethy.js

---
+++ controlflow/truethy.js
@@ -0,0 +1,52 @@
+// const userEmail = ""
+
+// if (userEmail) {
+//     console.log("Got user email");   
+// } else{
+//     console.log("Don't have user email");
+    
+// }
+
+// const userEmail1 = ""
+
+// if (userEmail1) {
+//     console.log("Got user email");   
+// } else{
+//     console.log("Don't have user email");
+    
+// }
+
+// const userEmail3 = {}
+
+// if (userEmail3) {
+//     console.log("Got user email");   
+// } else{
+//     console.log("Don't have user email");
+    
+// }
+
+// const userEmail4 = []
+
+// if (userEmail4) {
+//     console.log("Got user email");   
+// } else{
+//     console.log("Don't have user email");
+    
+// }
+
+// flasy value
+//    false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+// truethy value
+// "0", 'flase', " ", {}, [], function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty"); 
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+    
+}
