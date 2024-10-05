2838db0e89511bc993e46f4f8579492fb75b2623
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-10-05 13:12:36 +0000

    switch.js

---
+++ controlflow/switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("febuary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("january");
+//         break;
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "febuary":
+        console.log("febuary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        
+        break;
+}
