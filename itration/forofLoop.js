5ea8212fec13153cbcc59d259aaa7d354c097300
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-10-07 16:48:24 +0000

    forofLoop.js

---
+++ itration/forofLoop.js
@@ -0,0 +1,43 @@
+// for of loop
+
+// "", "", ""
+// {}, {}, {}
+
+// const arr = [1, 2, 3, 4 ,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Hello World!"
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greet}`); 
+// }
+
+// Map
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United State Amrican")
+map.set('Fr', "France")
+
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+
+// for (const [key, value] of map) {
+//     console.log(key, ":-", value);
+    
+// }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ":-", value);
+    
+// }
