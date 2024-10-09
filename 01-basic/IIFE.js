9a83d9e18ad41b0683c767a9fb1ae8809c43c7a9
Priya01Gupta <152805088+Priya01Gupta@users.noreply.github.com>
2024-10-09 08:30:46 -0700

    Delete 01-basic directory

--- 01-basic/IIFE.js
+++
@@ -1,9 +0,0 @@
-// Immediately Invoked Function Expression(IIFE)
-
-(function code(){
-    console.log(`DB CONNECTED`); 
-})();
-
-((name) => {
-    console.log(`DB CONNECTED TWO ${name}`); 
-})('Priya')
